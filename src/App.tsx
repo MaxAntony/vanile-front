fa85b33ed127934c5cb503ede43d3f59ab5e3a66
Max Antony <maxpacami@gmail.com>
2025-02-04 11:16:16 -0500

    feat: add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Button from "@mui/material/Button";
+
 function App() {
   return (
     <>
       <div className="bg-black text-white">
         <p>hola</p>
+        <Button variant="contained">Hello world</Button>;
       </div>
     </>
   );
