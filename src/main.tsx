3d682903f967abb63127622777057db09fbc3e6c
Max Antony <maxpacami@gmail.com>
2025-02-10 19:04:15 -0500

    feat: add auth contenxt

--- src/main.tsx
+++ src/main.tsx
@@ -8,12 +8,23 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 
 import './config/api';
+import { AuthProvider, useAuth } from './contexts/auth';
 import { routeTree } from './routeTree.gen';
 
-// Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+  defaultPreload: 'intent',
+  scrollRestoration: true,
+  context: {
+    auth: undefined!,
+  },
+});
 
-// Register the router instance for type safety
+export function InnerApp() {
+  const auth = useAuth();
+  return <RouterProvider router={router} context={{ auth }}></RouterProvider>;
+}
+
 declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router;
@@ -22,14 +33,15 @@ declare module '@tanstack/react-router' {
 
 const queryClient = new QueryClient();
 
-// Render the app
 const rootElement = document.getElementById('root')!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
+        <AuthProvider>
+          <InnerApp />
+        </AuthProvider>
       </QueryClientProvider>
       <CssBaseline />
     </StrictMode>,
